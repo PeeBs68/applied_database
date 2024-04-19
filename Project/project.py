9a347c2493df604e8b4beee4f272966a41faa99e
Phelim Barry <phelimb@yahoo.com>
2024-04-19 01:46:05 +0100

    Project Update

--- Project/project.py
+++ Project/project.py
@@ -247,11 +247,38 @@ def view_by_pop():
 		#print(x)
 	time.sleep(3)
 
-
+# Menu Item 6
 def view_twinned():
 	print("View Twinned Cities")
 	print ("Twinned Cities")
 	print ("--------------")
+	connect()
+	with driver.session() as session:
+		final = session.read_transaction(get_results)
+		for x in final:
+			print(x)
+			#time.sleep(1)
+		time.sleep(3)
+		#print(final)
+def connect():
+    global driver
+    uri = "neo4j://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "rootroot"))
+def get_results(tx):
+    query = "match(n:City)-[t:TWINNED_WITH]-(n1:City) return n.name, n1.name order by n.name"
+    names = []
+    names2 = []
+    results = tx.run(query)
+    for x in results:
+        names.append(x['n.name'])
+        names2.append(x['n1.name'])
+    x=0
+    final=[]
+    while x < len(names):
+        final.append(names[x] + " <-> " + names2[x])
+        x=x+1
+    return final
+
 
 def twinned_with_dublin():
 	print("Twinning with Dublin")
@@ -333,6 +360,10 @@ if __name__ == "__main__":
 	import click
 	# https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
 	from tabulate import tabulate
+	from neo4j import GraphDatabase
+
+	driver = None
+
 	db = mysql.connector.connect(
     host=cfg["host"],
     user = cfg["user"],
